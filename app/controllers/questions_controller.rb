83295fc618be05b14d02edb9cc040d995f089709
sergo <sergo8707@gmail.com>
2016-09-24 22:41:04 +0300

    update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -67,6 +67,10 @@ class QuestionsController < ApplicationController
   end
 
   def check_captcha(model)
-      verify_recaptcha(model: model) unless current_user.present?
+    if current_user.present?
+      verify_recaptcha(model: model)
+    else
+      true
+    end
   end
 end
